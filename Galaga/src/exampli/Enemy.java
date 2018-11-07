7df1f80a8b96a1574861443dbc549d177880f4c7
Raúl P. <raporpe@gmail.com>
2018-11-07 12:19:07 +0100

    Added enemy

---
+++ Galaga/src/exampli/Enemy.java
@@ -0,0 +1,19 @@
+package exampli;
+
+public class Enemy {
+
+	private int x;
+	private int y;
+	
+	public Enemy(int x, int y) {
+		
+		this.x = x;
+		this.y = y;
+		
+	}
+	
+	
+	
+	
+	
+}
--- Galaga/src/exampli/Main.java
+++ Galaga/src/exampli/Main.java
@@ -1,11 +1,24 @@
 package exampli;
+import java.util.Locale;
+import edu.uc3m.game.GameBoardGUI;
+
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
+		Locale.setDefault(new Locale("en"));
 		
+		GameBoardGUI board;
 		
+		board = new GameBoardGUI(17,22);
+		
+		board.setVisible(true);
+		
+		Enemy example = new Enemy(85,110);
+		
+
+	
 	}
 	
 	
