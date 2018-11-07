e1e2c5dfffbf1941265daf5377b1fd3aa58b9e8e
dgpMakes <43809495+dgpMakes@users.noreply.github.com>
2018-11-07 12:40:52 +0100

    te comiteo la vida

---
+++ Galaga/src/exampli/Enemy.java
@@ -0,0 +1,55 @@
+package exampli;
+
+public class Enemy {
+	private int x;
+	private int y;
+	private int id;
+
+	private String image;
+	
+	public Enemy (int x, int y, int id, String image) {
+		setX(x);
+		setY(y);
+		setId(id);
+		setImage(image);
+	}
+	
+	public void setX(int x) {
+		if(x>=0) {
+			this.x=x;
+		}
+	}
+	
+	public void setY(int y) {
+		if(y>=0) {
+			this.y=y;
+		}
+	}
+	
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	public void setImage(String im) {
+		this.image=im;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+	
+	
+	
+}
---
+++ Galaga/src/exampli/TestingGUI.java
@@ -0,0 +1,36 @@
+package exampli;
+//Importing the Locale object to change the default
+//configuration of the computer to English
+import java.util.Locale;
+//Importing the GameBoardGUI library
+import edu.uc3m.game.GameBoardGUI;
+public class TestingGUI {
+
+	public static void main(String[] args) {
+		//To change the GUI to English
+		Locale.setDefault(new Locale("en"));
+		//We declare a GameBoardGUI object
+		GameBoardGUI board;
+		//Create a 17x22 board
+		board = new GameBoardGUI(17,22);
+		//We create an enemy
+		Enemy enemy1= new Enemy(85,110,0,"enemy100.png");
+		//Adding the enemy to the board
+		board.gb_addSprite(enemy1.getId(), enemy1.getImage(), true);
+		//Placing the enemy at the requested coordinates
+		board.gb_moveSpriteCoord(enemy1.getId(), enemy1.getX(), enemy1.getX());
+		//set Sprite visible
+		board.gb_setSpriteVisible(enemy1.getId(), true);
+		//Make board visible
+		board.setVisible(true);
+		
+		do {
+			String lastAction;
+			lastAction = board.gb_getLastAction();
+			if(lastAction.equals("right")) {
+				int currentX = enemy1.getX();
+			}
+		}
+	}
+
+}
