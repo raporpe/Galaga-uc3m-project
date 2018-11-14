59d77045c945d74b2a666d42168df8f419d502da
dgpMakes <43809495+dgpMakes@users.noreply.github.com>
2018-11-14 12:03:16 +0100

    public static final constants

--- Galaga/src/exampli/Constants.java
+++ Galaga/src/exampli/Constants.java
@@ -1,20 +1,20 @@
 package exampli;
 
 public class Constants {
-	 final int DIR_N = 0;
-	 final int DIR_NNE = 1;
-	 final int DIR_NE = 2;
-	 final int DIR_ENE = 3;
-	 final int DIR_E = 4;
-	 final int DIR_ESE = 5;
-	 final int DIR_SE = 6;
-	 final int DIR_SSE = 7;
-	 final int DIR_S = 8;
-	 final int DIR_SSW = 9;
-	 final int DIR_SW = 10;
-	 final int DIR_WSW = 11;
-	 final int DIR_W = 12;
-	 final int DIR_WNW = 13;
-	 final int DIR_NW = 14;
-	 final int DIR_NNW = 15;
+	 public static final int DIR_N = 0;
+	 public static final int DIR_NNE = 1;
+	 public static final int DIR_NE = 2;
+	 public static final int DIR_ENE = 3;
+	 public static final int DIR_E = 4;
+	 public static final int DIR_ESE = 5;
+	 public static final int DIR_SE = 6;
+	 public static final int DIR_SSE = 7;
+	 public static final int DIR_S = 8;
+	 public static final int DIR_SSW = 9;
+	 public static final int DIR_SW = 10;
+	 public static final int DIR_WSW = 11;
+	 public static final int DIR_W = 12;
+	 public static final int DIR_WNW = 13;
+	 public static final int DIR_NW = 14;
+	 public static final int DIR_NNW = 15;
 }
