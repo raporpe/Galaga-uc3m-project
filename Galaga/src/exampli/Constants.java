c036c2748fa312e3907a24e3a4589a2d73a519c0
dgpMakes <43809495+dgpMakes@users.noreply.github.com>
2018-11-14 12:10:03 +0100

    constants committed again

--- Galaga/src/exampli/Constants.java
+++ Galaga/src/exampli/Constants.java
@@ -17,4 +17,24 @@ public class Constants {
 	 public static final int DIR_WNW = 13;
 	 public static final int DIR_NW = 14;
 	 public static final int DIR_NNW = 15;
+	 
+	// The move deltas for the directions: dX,dY}
+	 public static final int[][] MOVES = {
+	  { 0, -4 }, // DIR_N
+	  { 1, -4 }, // DIR_NNE
+	  { 3, -3 }, // DIR_NE
+	  { 4, -1 }, // DIR_ENE
+	  { 4, 0 }, // DIR_E
+	  { 4, 1 }, // DIR_ESE
+	  { 3, 3 }, // DIR_SE
+	  { 1, 4 }, // DIR_SSE
+	  { 0, 4 }, // DIR_S
+	  { -1, 4 }, // DIR_SSW
+	  { -3, 3 }, // DIR_SW
+	  { -4, 1 }, // DIR_WSW
+	  { -4, 0 }, // DIR_W
+	  { -4,-1 }, // DIR_WNW
+	  { -3,-3 }, // DIR_NW
+	  { -1,-4 }, // DIR_NNW
+	 };
 }
--- Galaga/src/exampli/Enemy.java
+++ Galaga/src/exampli/Enemy.java
@@ -42,6 +42,7 @@ public class Enemy {
 		return y;
 	}
 	
+
 	public int getId() {
 		return id;
 	}
