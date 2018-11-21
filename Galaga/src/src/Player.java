996c7138cf8f114ec84ce44db9595ca0ec354b67
dgpMakes <43809495+dgpMakes@users.noreply.github.com>
2018-11-21 12:45:16 +0100

    player edited

--- Galaga/src/src/Player.java
+++ Galaga/src/src/Player.java
@@ -2,5 +2,38 @@ package src;
 
 public class Player {
 	private int x;
-	private int y;
+	final int Y;
+	private String sprite;
+
+	public Player (int x, int y, String sprite) {
+		setX(x);
+		this.Y=y;
+		setSprite(sprite);
+	}
+	
+	public void setX(int x) {
+		if(x>=0) {
+			this.x=x;
+		}
+	}
+	
+	
+	public void setSprite(String sprite) {
+		this.sprite=sprite;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public String getSprite() {
+		return sprite;
+	}
+	
+	public void move(int module, int dir) {
+		setX(getX() + Constants.MOVES[dir][0]*module);
+		
+		setY(getY() + Constants.MOVES[dir][1]*module);
+		
+	}
 }
