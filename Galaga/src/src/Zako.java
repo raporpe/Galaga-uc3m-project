0ae5ebac820ae32b00a21bc92e63a93104ef7c39
Raúl P. <raporpe@gmail.com>
2018-11-27 19:51:56 +0100

    Improved abstract

--- Galaga/src/src/Zako.java
+++ Galaga/src/src/Zako.java
@@ -1,9 +1,14 @@
 package src;
 
-public  class Zako extends Enemy{
-	private String imgPath="enemy300.png";
+public class Zako extends Enemy{
+	
+	private String imgPath = "enemy300.png";
+	
 	public Zako(int id){
+		
 		super();
+		
 		this.imgPath = getImgPath();
+		
 	}
 }
