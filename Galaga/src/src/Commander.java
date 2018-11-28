442f87f19fd1c9a657dc1b0f678c2048b9da0833
dgpMakes <43809495+dgpMakes@users.noreply.github.com>
2018-11-28 12:39:58 +0100

    updated commander and goei

--- Galaga/src/src/Commander.java
+++ Galaga/src/src/Commander.java
@@ -7,4 +7,11 @@ public class Commander extends Enemy{
 	public Commander(int id, GameBoardGUI board){
 		super(id,board,"enemy100.png");
 	}
+	public static void moveZako(Enemy commander) {
+		commander.x = commander.getX()+(int) (Math.random()*10);
+	}
+	
+	public static void damaged(Enemy commander) {
+		commander.imgPath="enemy900.png";
+	}
 }
