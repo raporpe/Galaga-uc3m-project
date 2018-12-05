a4b8f3a3e57ddea5d52ae27cdc2a4a15a91f743f
Raúl P. <raporpe@gmail.com>
2018-12-05 11:25:54 +0100

    Not going to work

---
+++ Galaga/src/src/PlayerTorpedo.java
@@ -0,0 +1,12 @@
+package src;
+
+import edu.uc3m.game.GameBoardGUI;
+
+public class PlayerTorpedo extends Torpedo{
+
+	
+	public PlayerTropedo(int id, GameBoardGUI board, ) {
+		super(id, board, "torpedo100.png", "",  0, Player.);
+	}
+	
+}
